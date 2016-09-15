09be84a381b3003ed8c8253db0ecc3d8ff42a9c6
JordiTeddy <jorditeddy@gmail.com>
2016-09-15 18:59:52 +0200

    add dynamic typing

--- fetchSpreadsheet.js
+++ fetchSpreadsheet.js
@@ -4,7 +4,10 @@ const fetch = require('node-fetch');
 const getUrl = (id) => ( 'https://docs.google.com/spreadsheets/d/' + id + '/pub?output=csv');
 //  1OgDtJt_hsVKOz03E59BV-HASxqv2eGiuL__KEQMaLT4
 
-const makePapa = (text) => papa.parse(text, {header:true});
+const makePapa = (text) => papa.parse(text, {
+	header:true,
+	dynamicTyping: true
+});
 
 const fetchSpreadsheet = (id) => {
   console.log('fetching', id);
