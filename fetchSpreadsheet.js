21e3e5d0d10b50ea5d34f73387a2af94e6b4a608
JSicart <jsicart@intercalleurope.com>
2016-09-15 12:22:16 +0200

    resolve bug with header column not taken into account

--- fetchSpreadsheet.js
+++ fetchSpreadsheet.js
@@ -4,11 +4,7 @@ const fetch = require('node-fetch');
 const getUrl = (id) => ( 'https://docs.google.com/spreadsheets/d/' + id + '/pub?output=csv');
 //  1OgDtJt_hsVKOz03E59BV-HASxqv2eGiuL__KEQMaLT4
 
-const makePapa = (text) => papa.parse(text, {
-      config: {
-        header:true,
-      },
-    });
+const makePapa = (text) => papa.parse(text, {header:true});
 
 const fetchSpreadsheet = (id) => {
   console.log('fetching', id);
